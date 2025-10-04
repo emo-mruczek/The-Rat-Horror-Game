c67d1db6d92e10f07dfc2dbf918d671270a08852
Rafau <r@rafau.pl>
2025-10-04 22:02:46 +0200

    light updates

--- scripts/player_flashlight.gd
+++ scripts/player_flashlight.gd
@@ -1,5 +1,6 @@
 extends Node
 
+
 func _on_player_flashlight_clicked() -> void:
     var player = get_tree().get_first_node_in_group("player")
     if player.has_flashlight:
@@ -7,12 +8,10 @@ func _on_player_flashlight_clicked() -> void:
     else:
         $SpotLight3D.visible = false
 
-
 func _on_area_3d_body_entered(body: Node3D) -> void:
     if body.is_in_group("mob"):
         var enemy: Mob = body
         enemy.stop_in_light()
-
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
     if body.is_in_group("mob"):
