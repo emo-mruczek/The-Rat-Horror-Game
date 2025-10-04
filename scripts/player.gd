996fc5612a12b1a3b12636ac4fbff86532ce23e6
MicroPanda123 <me@markasspandi.pl>
2025-10-04 19:05:21 +0200

    togglable player_flashlight

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,12 +2,13 @@ class_name Player
 
 extends CharacterBody3D
 
+signal flashlight_clicked
+
 @export
 var has_flashlight = true
 
 const SPEED = 3.0
 const JUMP_VELOCITY = 4.5
-
 
 func _physics_process(delta: float) -> void:
     # Add the gravity.
@@ -17,6 +18,11 @@ func _physics_process(delta: float) -> void:
     # Handle jump.
     if Input.is_action_just_pressed("ui_accept") and is_on_floor():
         velocity.y = JUMP_VELOCITY
+
+    if Input.is_action_just_pressed("flashlight"):
+        print("pre signal")
+        has_flashlight = not has_flashlight
+        flashlight_clicked.emit()
 
     # Get the input direction and handle the movement/deceleration.
     # As good practice, you should replace UI actions with custom gameplay actions.
--- scripts/player_flashlight.gd
+++ scripts/player_flashlight.gd
@@ -1,10 +1,8 @@
 extends Node
 
-func _process(_delta: float) -> void:
-    var playerArray: Player = get_tree().get_first_node_in_group("player")
-    if playerArray == null:
-        return       
-    if playerArray.has_flashlight:
+func _on_player_flashlight_clicked() -> void:
+    var player = get_tree().get_first_node_in_group("player")
+    if player.has_flashlight:
         $SpotLight3D.visible = true
     else:
         $SpotLight3D.visible = false
