3b36674716ad56c4aaba26d2ab8f75781eaa1c44
poz <poz@poz.pet>
2025-10-04 23:28:12 +0200

    fully functional flashlight

--- scripts/player_flashlight.gd
+++ scripts/player_flashlight.gd
@@ -3,6 +3,7 @@ extends Node
 
 func _on_player_flashlight_clicked() -> void:
     $SpotLight3D.visible = not $SpotLight3D.visible
+    $FlashlightLight.visible = not $FlashlightLight.visible
     AudioManager.play("res://assets/flashlight.wav")
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
