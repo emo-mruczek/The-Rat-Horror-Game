7dbae75723163dbd8053588a60092dba9cf88cde
MicroPanda123 <me@markasspandi.pl>
2025-10-05 09:42:44 +0200

    door 2

--- scripts/door_2.gd
+++ scripts/door_2.gd
@@ -3,13 +3,13 @@ extends Node
 const key_id = "key_2"
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if body.is_in_group("player"):
-		var player: Player = body
-		if player.holding_item == key_id:
-			queue_free()
-		
+    if body.is_in_group("player"):
+        var player: Player = body
+        if player.holding_item == key_id:
+            queue_free()
+        
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
-	if body.is_in_group("player"):
-		var player: Player = body
-		print("drzwi2!!!")
+    if body.is_in_group("player"):
+        var player: Player = body
+        print("drzwi2!!!")
