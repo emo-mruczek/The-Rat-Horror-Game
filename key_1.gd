ef003abf6686c3c476c4866bd8c85ff74b4b94d3
MicroPanda123 <me@markasspandi.pl>
2025-10-05 09:33:39 +0200

    door scale

--- key_1.gd
+++ key_1.gd
@@ -3,6 +3,6 @@ extends PickableBody
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	area = $PickableArea
-	$PlayerKey1.set("shaded", true)
-	$PlayerKey1.set("modulate", Color(1,1,1,1))
+    area = $PickableArea
+    $PlayerKey1.set("shaded", true)
+    $PlayerKey1.set("modulate", Color(1,1,1,1))
--- pickable_area.gd
+++ pickable_area.gd
@@ -3,11 +3,11 @@ extends Area3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
-	#var player: Player = get_overlapping_bodies()[0]
-	pass
-		
+    #var player: Player = get_overlapping_bodies()[0]
+    pass
+        
