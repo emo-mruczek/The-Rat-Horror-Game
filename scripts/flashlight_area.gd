8d633515918ad5bbe89a342eac1d4c8df94871a7
MicroPanda123 <me@markasspandi.pl>
2025-10-05 11:20:45 +0200

    doors

--- scripts/flashlight_area.gd
+++ scripts/flashlight_area.gd
@@ -2,9 +2,10 @@ extends Area3D
 
 
 func _physics_process(_delta: float) -> void:
+    if (!monitoring):
+        return
     var bodies = get_overlapping_bodies()
     for body in bodies:
         if body.is_in_group("mob"):
             var mob: Mob = body
             mob.stop_in_light()
-            
